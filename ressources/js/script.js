559d4a412efd7bd7d4e782809feeadff39b3de48
Johan <njoubert06@gmail.com>
2020-10-13 22:56:14 +0200

    all section

--- ressources/js/script.js
+++ ressources/js/script.js
@@ -1,4 +1,5 @@
 const nav = document.querySelector('nav');
+const link = document.querySelector('.nav-link');
 
 
 window.addEventListener('scroll', () => {
@@ -9,3 +10,28 @@ window.addEventListener('scroll', () => {
     nav.classList.remove('scroll');
   }
 })
+
+
+
+//translate effect
+
+$(document).ready(function(){
+  $(window).on('scroll', function () {
+  
+  var elmt   = $('.from-left, .from-right');
+  var topImg = $('.from-left, .from-right').offset().top;
+  var scroll = $(window).scrollTop();
+  
+      $(elmt).each(function() {
+          
+          var topImg = $(this).offset().top - 300;
+          
+              if ( topImg < scroll ) {
+              
+              $(this).css("transform", "translate(0,0)");
+              $(this).css("opacity", "1");
+              
+              };
+      });
+  });
+});
