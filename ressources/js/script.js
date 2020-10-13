1330dc1fbc66e380e32fef711e34dcd4095a1fdf
Johan Joubert <njoubert06@gmail.com>
2020-10-13 13:37:49 +0100

    add compétence

---
+++ ressources/js/script.js
@@ -0,0 +1,11 @@
+const nav = document.querySelector('nav');
+
+
+window.addEventListener('scroll', () => {
+  if(window.scrollY > 500) {
+    nav.classList.add('scroll');
+  }
+  else {
+    nav.classList.remove('scroll');
+  }
+})
